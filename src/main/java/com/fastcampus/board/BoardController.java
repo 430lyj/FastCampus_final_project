211643612e5aedc32ef1cdf3f3057abc84e3ae88
430lyj <430lyj@gmail.com>
2022-02-10 19:56:51 +0900

    feat: crud controller 기능 개발

--- src/main/java/com/fastcampus/board/BoardController.java
+++ src/main/java/com/fastcampus/board/BoardController.java
@@ -1,36 +1,47 @@
 package com.fastcampus.board;
 
+import com.fastcampus.board.domain.Board;
 import com.fastcampus.board.dto.PostDto;
+import com.fastcampus.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
+@RequiredArgsConstructor
 public class BoardController {
     // TODO BoardController는 JSP를 이용한 화면 로직과 연결되어 있습니다.
     //  다른 부분은 바꾸지 말고 TODO 부분만 작성해주시기를 권장합니다 :)
     // TODO please write code on only TODO part in BoardController
 
-    @RequestMapping(value="/save",method = RequestMethod.POST)
+    private final BoardRepository boardRepository;
+
+    @RequestMapping(value="/save", method = RequestMethod.POST)
     public String createPost(@ModelAttribute("command") PostDto postDto){
-        System.out.println("save " + postDto);
         /* TODO: 게시물 추가 로직*/
+        Board board = new Board();
+        board.createPost(postDto.getTitle(), postDto.getNickName(), postDto.getContent(), 0);
+        boardRepository.save(board);
         return "redirect:/"; // 추가 후 홈 화면으로
     }
 
     @RequestMapping("/")
     public String ReadAllPost(Model model){
-        // List<PostDto> postList = /* TODO 게시물 전체를 받아오는 로직 */
+        /* TODO 게시물 전체를 받아오는 로직 */
+        Iterable<Board> boards = boardRepository.findAll();
 
-        // ********************************************
-        // 게시물을 받아오는 로직을 작성한 뒤엔 아래 2줄은 삭제 해주셔도 됩니다.
         List<PostDto> postList = new ArrayList<>();
-        postList.add(new PostDto(10, "hey", "새 글", "예제"));
-        // *****************************************
-
+        for (Board board : boards) {
+            postList.add(new PostDto(board.getId(), board.getWriter(), board.getTitle(), board.getContent()));
+            board.upCnt();
+        }
+        boardRepository.saveAll(boards);
         model.addAttribute("postList", postList);
         return "index";
     }
@@ -39,13 +50,26 @@ public class BoardController {
     public String updatePost(@ModelAttribute("command") PostDto postDto){
         System.out.println("update " + postDto);
         /* TODO 게시물 수정 로직 */
+        Long postId = postDto.getPostId();
+        Optional<Board> board = boardRepository.findById(postId);
+        if (board.isPresent()){
+            Board realBoard = board.get();
+            realBoard.updatePost(postDto.getTitle(), postDto.getNickName(), postDto.getContent());
+            realBoard.upCnt();
+            boardRepository.save(realBoard);
+        }
         return "redirect:/";
     }
 
-    @RequestMapping(value="/delete/{id}",method = RequestMethod.GET)
-    public String deletePost(@PathVariable int id){
+    @RequestMapping(value="/delete/{id}", method = RequestMethod.GET)
+    public String deletePost(@PathVariable Long id){
         System.out.println("삭제 " + id);
         /* TODO 게시물 삭제 로직 */
+        Optional<Board> toBeDeleted = boardRepository.findById(id);
+        if (toBeDeleted.isPresent()){
+            Board board = toBeDeleted.get();
+            boardRepository.delete(board);
+        }
         return "redirect:/";
     }
 
@@ -60,7 +84,7 @@ public class BoardController {
     }
 
     @RequestMapping(value="/updateView/{id}")
-    public String ViewUpdate(@PathVariable int id, Model model){
+    public String ViewUpdate(@PathVariable Long id, Model model){
         PostDto postDto = new PostDto();
         postDto.setPostId(id);
         model.addAttribute("command",postDto);
--- src/main/java/com/fastcampus/board/domain/Board.java
+++ src/main/java/com/fastcampus/board/domain/Board.java
@@ -1,11 +1,16 @@
 package com.fastcampus.board.domain;
 
+import com.fastcampus.board.dto.PostDto;
+import lombok.Builder;
 import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.sql.Time;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
@@ -13,7 +18,7 @@ import java.util.Date;
 @Getter
 public class Board {
 
-    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "seq")
     private Long id;
 
@@ -28,6 +33,32 @@ public class Board {
 
     private int cnt;
 
-    @DateTimeFormat(pattern = "MM/dd/yyyy")
-    private Timestamp reg_date;
+    @CreationTimestamp
+    private LocalDateTime reg_date;
+
+    /**
+     * 포스트 생성
+     */
+    public void createPost(String title, String writer, String content, int cnt){
+        this.title = title;
+        this.writer = writer;
+        this.content = content;
+        this.cnt = cnt;
+    }
+
+    /**
+     * 포스트 업데이트
+     */
+    public void updatePost(String title, String writer, String content){
+        this.title = title;
+        this.writer = writer;
+        this.content = content;
+    }
+
+    /**
+     * cnt += 1
+     */
+    public void upCnt(){
+        this.cnt ++;
+    }
 }
